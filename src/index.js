02b149b7f60c902807df080ae94eed8e05ef148f
최광희 <polyckh@gmail.com>
2021-12-03 15:15:44 +0900

    feat: car-names-submit 버튼을 누르면 car-names-input에 입력된 car names 받기

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import Car from './Car.js';
-function RacingGame() {}
+import { $ } from './utils/dom.js';
+function RacingGame() {
+  this.cars = [];
+  this.init = () => {
+    initEventHandler();
+  };
+  const setCarNames = () => {
+    const carNamesArray = getCarNames().split(',');
+    this.cars = carNamesArray;
+  };
+  const getCarNames = () => $('#car-names-input').value;
+  const initEventHandler = () => {
+    $('#car-names-form').addEventListener('submit', (e) => e.preventDefault());
+    $('#racing-count-form').addEventListener('submit', (e) =>
+      e.preventDefault(),
+    );
+    $('#car-names-submit').addEventListener('click', setCarNames);
+  };
+}
+const racingGame = new RacingGame();
+racingGame.init();
