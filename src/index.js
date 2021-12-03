a68ebea3709bdc086c3920f02416f39ff10a67a8
최광희 <polyckh@gmail.com>
2021-12-03 15:20:42 +0900

    feat: 잘못된 자동차 이름 alert 처리

--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ function RacingGame() {
   };
   const setCarNames = () => {
     const carNamesArray = getCarNames().split(',');
+    if (!isValidCarNames(carNamesArray)) {
+      alert('자동차 이름은 5글자 이하로 입력해 주세요.');
+      $('#car-names-input').value = '';
+      $('#car-names-input').focus();
+    }
     this.cars = carNamesArray;
   };
+  const isValidCarNames = (carNamesArray) =>
+    carNamesArray.every((carName) => carName.length <= 5);
   const getCarNames = () => $('#car-names-input').value;
+
   const initEventHandler = () => {
     $('#car-names-form').addEventListener('submit', (e) => e.preventDefault());
     $('#racing-count-form').addEventListener('submit', (e) =>
