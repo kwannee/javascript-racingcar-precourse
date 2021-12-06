16c2d1682eec629452306eef2674047125914564
최광희 <polyckh@gmail.com>
2021-12-06 21:33:01 +0900

    refactor: valid.js 함수 객체화

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import Car from './Car.js';
 import { $, getElementValue, formsPreventDefault } from './utils/dom.js';
 import { ELEMENT } from './constant/index.js';
-import {
-  checkValidCarNames,
-  checkValidRacingCount,
-  checkCarsExist,
-} from './utils/valid.js';
+import { VALID } from './utils/valid.js';
 import { raceAllCars, getRacingWinner } from './racing.js';
 import {
   createRacingResultsElement,
@@ -22,18 +18,18 @@ function RacingGame() {
 
   const setCarNames = () => {
     const carNamesArray = getElementValue(ELEMENT.carNamesInputID).split(',');
-    if (!checkValidCarNames(carNamesArray)) {
+    if (!VALID.checkValidCarNames(carNamesArray)) {
       return;
     }
     this.cars = carNamesArray.map((carName) => new Car(carName));
   };
 
   const submitRacingCount = () => {
-    if (!checkCarsExist(this.cars)) {
+    if (!VALID.checkCarsExist(this.cars)) {
       return;
     }
     const racingCount = getElementValue(ELEMENT.racingCountInputID);
-    if (!checkValidRacingCount(racingCount)) {
+    if (!VALID.checkValidRacingCount(racingCount)) {
       return;
     }
     startRacingGame(racingCount);
@@ -50,10 +46,10 @@ function RacingGame() {
 
   const renderRacingResults = () => {
     const racingResultsElement = createRacingResultsElement(this.results);
-    $('#app').appendChild(racingResultsElement);
+    $(ELEMENT.appID).appendChild(racingResultsElement);
     const winners = getRacingWinner(this.cars);
     const racingWinnersElement = createRacingWinnersElement(winners);
-    $('#app').appendChild(racingWinnersElement);
+    $(ELEMENT.appID).appendChild(racingWinnersElement);
   };
 
   const initEventHandler = () => {
--- src/utils/valid.js
+++ src/utils/valid.js
@@ -2,29 +2,31 @@ import { $ } from './dom.js';
 import { ELEMENT, ALERT_MESSAGE } from '../constant/index.js';
 import { MAX_CAR_NAME_LENGTH } from '../constant/index.js';
 import { clearInput } from './dom.js';
-export const checkValidRacingCount = (racingCount) => {
-  if (!isPositiveNumber(racingCount)) {
-    alert(ALERT_MESSAGE.WRONG_COUNT_NUMBER);
-    clearInput(racingCountInputID);
-    return false;
-  }
-  return true;
-};
-export const checkCarsExist = (cars) => {
-  if (!cars.length) {
-    alert(ALERT_MESSAGE.CAR_NOT_EXIT);
-    $(ELEMENT.carNamesInputID).focus();
-    return false;
-  }
-  return true;
-};
-export const checkValidCarNames = (carNamesArray) => {
-  if (!isValidCarNamesLength(carNamesArray)) {
-    alert(ALERT_MESSAGE.WRONG_CAR_NAME_LENGTH);
-    clearInput(ELEMENT.carNamesInputID);
-    return false;
-  }
-  return true;
+export const VALID = {
+  checkValidRacingCount(racingCount) {
+    if (!isPositiveNumber(racingCount)) {
+      alert(ALERT_MESSAGE.WRONG_COUNT_NUMBER);
+      clearInput(racingCountInputID);
+      return false;
+    }
+    return true;
+  },
+  checkCarsExist(cars) {
+    if (!cars.length) {
+      alert(ALERT_MESSAGE.CAR_NOT_EXIT);
+      $(ELEMENT.carNamesInputID).focus();
+      return false;
+    }
+    return true;
+  },
+  checkValidCarNames(carNamesArray) {
+    if (!isValidCarNamesLength(carNamesArray)) {
+      alert(ALERT_MESSAGE.WRONG_CAR_NAME_LENGTH);
+      clearInput(ELEMENT.carNamesInputID);
+      return false;
+    }
+    return true;
+  },
 };
 const isPositiveNumber = (number) => number > 0;
 const isValidCarNamesLength = (carNamesArray) =>
